14c451d7e055248f0c787f40bae41d554a0edfaf
Alinnaq1 <91326277+Alinnaq1@users.noreply.github.com>
2021-10-10 13:17:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('ODYzNzUzMzQ1NjAwMzIzNTk0.YOre_Q.gJKnYGd2elfLNlsVSCjSFvebP2U');
+client.login('ODYzNzUzMzQ1NjAwMzIzNTk0.YOre_Q.j5RXMxIVvYRD4OKHWy2KQ2jRTxo');
 client.commands = new Collection();
 client.setMaxListeners(0);    
 client.prefix = PREFIX;
